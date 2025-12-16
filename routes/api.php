7e6358794cadd946bf4dda769767f4cac2697aa2
arbim6 <arbiim26@gmail.com>
2025-12-16 15:22:24 +0700

    Add artisan

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/ping', function () {
     return response()->json([
@@ -19,4 +20,12 @@ Route::middleware('auth:api')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('me', [AuthController::class, 'me']);
+});
+
+Route::get('/__migrate_fresh', function () {
+    Artisan::call('migrate:fresh', ['--force' => true]);
+    return response()->json([
+        'status' => 'ok',
+        'output' => Artisan::output()
+    ]);
 });
